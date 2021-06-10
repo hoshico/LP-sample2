26ccb488fc6b74f93d887b595bc5b8cbce39c529
hoshico <melancholinista.yasu@gmail.com>
2021-06-10 09:11:12 +0900

    ScrollReveal.js実装

---
+++ main.js
@@ -0,0 +1,30 @@
+ScrollReveal().reveal('.img_1_left',{
+  duration: 800,
+  viewFactory: 0.2,
+  reset: true
+});
+ScrollReveal().reveal('.img_1_right',{
+  duration: 800,
+  viewFactory: 0.2,
+  reset: true
+});
+ScrollReveal().reveal('.img_2_left',{
+  duration: 800,
+  viewFactory: 0.2,
+  reset: true
+});
+ScrollReveal().reveal('.img_2_right',{
+  duration: 800,
+  viewFactory: 0.2,
+  reset: true
+});
+ScrollReveal().reveal('.img_3_left',{
+  duration: 800,
+  viewFactory: 0.2,
+  reset: true
+});
+ScrollReveal().reveal('.img_3_right',{
+  duration: 800,
+  viewFactory: 0.2,
+  reset: true
+});
